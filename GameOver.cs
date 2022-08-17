cef3a38d631e5ef42d5e25f6b880c236e86103ac
Peter Ho <103221084+PeterQHo@users.noreply.github.com>
2022-08-17 09:05:53 -0700

    Add files via upload

---
+++ GameOver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject gameOverMenu;
+
+    private void OnEnable()
+    {
+        FighterStats.OnPlayerDeath += EnableGameOverMenu;
+    }
+
+    private void OnDisable()
+    {
+        FighterStats.OnPlayerDeath -= EnableGameOverMenu;
+    }
+
+    public void EnableGameOverMenu()
+    {
+        gameOverMenu.SetActive(true);
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
